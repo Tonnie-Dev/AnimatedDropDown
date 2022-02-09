21ec9a36cb969c9bae0788c6586d542a917e0701
Antony <vontonnie@gmail.com>
2022-02-09 07:22:00 +0300

    Animate alpha

--- app/src/main/java/com/uxstate/animateddropdown/MainActivity.kt
+++ app/src/main/java/com/uxstate/animateddropdown/MainActivity.kt
@@ -3,7 +3,9 @@ package com.uxstate.animateddropdown
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.runtime.*
 import com.uxstate.animateddropdown.ui.theme.AnimatedDropDownTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,6 +21,15 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun DropDown(text: String, content: @Composable () -> Unit, isOpen: Boolean) {
+fun DropDown(text: String, content: @Composable () -> Unit, initiallyOpened: Boolean) {
+
+
+    var isOpen by remember { mutableStateOf(initiallyOpened) }
+
+
+    val alpha = animateFloatAsState(
+        targetValue = if (isOpen) 1f else 0f,
+        animationSpec = tween(durationMillis = 100)
+    )
 
 }
